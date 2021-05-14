3b9043786ec922f4738ca666a2b54b0506aa48cd
Nick Flores-Tober <nflores161@gmail.com>
2021-05-14 18:21:31 -0400

    Updated CSS

--- cat-fight-app/src/CharacterCard.js
+++ cat-fight-app/src/CharacterCard.js
@@ -5,11 +5,11 @@ import Col from 'react-bootstrap/Col'
 const CharacterCard = (props) => {
     return(
         <Col className="blackBackground catRow">
-            <Card className="warriorCard blackBackground">
+            <Card className="warriorCard">
                 <Card.Img className="catCardImage" src={props.cat.image} alt={props.cat.name} />
                 <Card.Body className="catCardBody blackBackground">
-                <Card.Title>{props.cat.name}</Card.Title>
-                <Card.Text className="purrsonalityText">{props.cat.purrsonality}</Card.Text>
+                <Card.Title className="blackBackground">{props.cat.name}</Card.Title>
+                <Card.Text className="purrsonalityText blackBackground">{props.cat.purrsonality}</Card.Text>
                 <Button variant="outline-light" onClick={() => {
                     props.assignCat(props.cat)
                     props.history.push("/battleground")
